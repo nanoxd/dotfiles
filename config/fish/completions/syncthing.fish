94c8f77d1b332974ae5355f4da730c250a5a1aa2
Fernando Paredes <nano@fdp.io>
2015-04-10 20:13:09 -0700

    [Fish] Remove syncthing completion

--- config/fish/completions/syncthing.fish
+++
@@ -1,7 +0,0 @@
-# syncthing.fish created by Fernando Paredes<nano@fdp.io>
-
-complete -c syncthing -f -o home= -d "Set configuration directory"
-complete -c syncthing -A -f -o logflags= -d "Set log flags"
-complete -c syncthing -A -f -o reset -d "Prepare to resync from cluster"
-complete -c syncthing -A -f -o upgrade -d "Perform upgrade"
-complete -c syncthing -A -f -o version -d "Show version"
