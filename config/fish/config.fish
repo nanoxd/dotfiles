1822c772f11250a9a8cdeb3ea48ad3aedc84adcd
Fernando Paredes <nano@fdp.io>
2015-04-30 21:07:03 -0700

    [fish] Add tmux creation with ssh

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -29,3 +29,12 @@ end
 
 # Use nvm
 . $HOME/.config/fish/wrappers/nvm.fish
+
+# SSH nonsense
+if status --is-login
+  set PPID (echo (ps --pid %self -o ppid --no-headers) | xargs)
+  if ps --pid $PPID | grep ssh
+    tmux has-session -t remote; and tmux attach-session -t remote; or tmux new-session -s remote; and kill %self
+    echo "tmux failed to start; using plain fish shell"
+  end
+end
