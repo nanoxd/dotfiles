24a731a1ddacff8c73f7b484784f678695fdf680
Fernando Paredes <nano@fdp.io>
2016-05-02 19:49:09 -0700

    [zsh] Add git helper

---
+++ zsh/git.zsh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+eval "$(hub alias -s)"
+
+alias ga='git add'
+alias gaa='git add --all'
+alias gaap='git add --all --patch'
+alias gc='git commit -v'
+alias gca='git commit -v --amend'
+alias gcb='git create-branch'
+alias gcl='git clone'
+alias gco='git checkout'
+alias gd='git diff'
+alias gdc='git diff --cached'
+alias gp='git push'
+alias gpr='git pull-request --browse'
+alias gpu='git pull --rebase'
+alias gs='git status -sb'
