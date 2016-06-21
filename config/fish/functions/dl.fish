e47870055c3df10b46b488ea483ad82786f6419e
Fernando Paredes <nano@fdp.io>
2016-06-21 06:50:50 -0700

    [Fish] Remove Docker functions

--- config/fish/functions/dl.fish
+++
@@ -1,3 +0,0 @@
-function dl -d "docker last id"
-  docker ps -l -q
-end
--- config/fish/functions/docker-ip.fish
+++
@@ -1,3 +0,0 @@
-function docker-ip -d "Docker: Get IP address"
-  docker inspect (dl) | grep IPAddress | cut -d '""' -f 4
-end
