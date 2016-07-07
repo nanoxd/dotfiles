8c58755706ce386efd523cdb3f07d6ac7f9fef50
Fernando Paredes <nano@fdp.io>
2016-07-07 15:07:57 -0700

    [Zsh] True case insensitive completion

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -9,13 +9,23 @@ _force_rehash() {
 zstyle ':completion:*' completer _oldlist _expand _force_rehash _complete _match _ignored _approximate
 
 # matches case insensitive for lowercase
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+zstyle ':completion:*' menu select
 
-# menu if nb items > 2
-zstyle ':completion:*' menu select=2
+### fuzzy completion
+# lifted from http://superuser.com/a/815317/555734
+# 0 -- vanilla completion (abc => abc)
+# 1 -- smart case completion (abc => Abc)
+# 2 -- word flex completion (abc => A-big-Car)
+# 3 -- full flex completion (abc => ABraCadabra)
+zstyle ':completion:*' matcher-list '' \
+  'm:{a-z\-}={A-Z\_}' \
+  'r:[^[:alpha:]]||[[:alpha:]]=** r:|=* m:{a-z\-}={A-Z\_}' \
+  'r:[[:ascii:]]||[[:ascii:]]=** r:|=* m:{a-z\-}={A-Z\_}'
 
 setopt auto_menu
 setopt autolist
+setopt menu_complete
