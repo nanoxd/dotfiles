2bc41f44873a3aa3649bbcad4e945e61352ab628
Fernando Paredes <nano@fdp.io>
2016-09-29 20:34:12 -0700

    Setup hammerspoon

---
+++ hammerspoon/init.lua
@@ -0,0 +1,47 @@
+-- A global variable for the Hyper Mode
+k = hs.hotkey.modal.new({}, "F17")
+
+-- Hyper+key for all the below are setup somewhere
+-- The handler already exists, usually in Keyboard Maestro
+-- we just have to get the right keystroke sent
+hyperBindings = {'s','t','SPACE'}
+
+for i,key in ipairs(hyperBindings) do
+  k:bind({}, key, nil, function() hs.eventtap.keyStroke({'cmd','alt','shift','ctrl'}, key)
+    k.triggered = true
+  end)
+end
+
+-- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
+pressedF18 = function()
+  k.triggered = false
+  k:enter()
+end
+
+-- Leave Hyper Mode when F18 (Hyper/Capslock) is pressed,
+--   send ESCAPE if no other keys are pressed.
+releasedF18 = function()
+  k:exit()
+  if not k.triggered then
+    hs.eventtap.keyStroke({}, 'ESCAPE')
+  end
+end
+
+-- Bind the Hyper key
+f18 = hs.hotkey.bind({}, 'F18', pressedF18, releasedF18)
+
+-- Reload config when any lua file in config directory changes
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == '.lua' then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+
+local myWatcher = hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', reloadConfig):start()
+hs.alert.show('Config loaded')
