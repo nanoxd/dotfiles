dadb78e94d046dfb7f3fd2f9497343403de1b701
Fernando Paredes <nano@fdp.io>
2016-10-01 13:37:08 -0700

    [Hammerspoon] Refactor hyper into seperate file

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,34 +1,6 @@
--- A global variable for the Hyper Mode
-k = hs.hotkey.modal.new({}, "F17")
+local hyper = require "hyper"
 
--- Hyper+key for all the below are setup somewhere
--- The handler already exists, usually in Keyboard Maestro
--- we just have to get the right keystroke sent
-hyperBindings = {'s','t','SPACE'}
-
-for i,key in ipairs(hyperBindings) do
-  k:bind({}, key, nil, function() hs.eventtap.keyStroke({'cmd','alt','shift','ctrl'}, key)
-    k.triggered = true
-  end)
-end
-
--- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
-pressedF18 = function()
-  k.triggered = false
-  k:enter()
-end
-
--- Leave Hyper Mode when F18 (Hyper/Capslock) is pressed,
---   send ESCAPE if no other keys are pressed.
-releasedF18 = function()
-  k:exit()
-  if not k.triggered then
-    hs.eventtap.keyStroke({}, 'ESCAPE')
-  end
-end
-
--- Bind the Hyper key
-f18 = hs.hotkey.bind({}, 'F18', pressedF18, releasedF18)
+hyper.init()
 
 -- Reload config when any lua file in config directory changes
 function reloadConfig(files)
