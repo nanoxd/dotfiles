096983662eab8814c440f75fe7c45b15324fec75
Fernando Paredes <nano@fdp.io>
2016-10-07 14:56:19 -0700

    [Fish] Add fzf tmux option

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,6 +7,7 @@ set -gx PAGER less
 # ENVs
 set -x GOPATH "$HOME/Dev/go"
 set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
+set -U FZF_TMUX 1
 set -x SLACK_URL "https://hooks.slack.com/services/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"
 
 # Set paths
