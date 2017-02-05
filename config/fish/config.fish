cb1c5bdcf5194f7903888fe3f0e1cb5f7d5894ae
Fernando Paredes <nano@fdp.io>
2017-02-05 11:15:59 -0800

    [Fish] Use postgresapp

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -35,6 +35,10 @@ end
 
 export LIBRARY_PATH="$LIBRARY_PATH:/usr/local/lib"
 
+if -test -d "/Applications/Postgres.app/Contents/Versions/latest/bin"
+  set -U fish_user_paths "/Applications/Postgres.app/Contents/Versions/latest/bin" $fish_user_paths
+end
+
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
