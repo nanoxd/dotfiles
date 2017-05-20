108f301b3297f7ad3e96723ad6a94af7f5adeeae
Fernando Paredes <nano@fdp.io>
2017-05-20 11:23:19 -0700

    [hyper] Add hyper-pane and summon shortcut

--- hyper.js
+++ hyper.js
@@ -38,9 +38,7 @@ module.exports = {
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
     // if left empty, your system's login shell will be used by default
     shell: '',
-
-    // for advanced config flags please refer to https://hyperterm.org/#cfg
-    // summonShortcut: 'Cmd+Ctrl+Option+Shift+t',
+    summonShortcut: 'Cmd+Ctrl+Option+Shift+t',
   },
 
   plugins: [
@@ -48,6 +46,7 @@ module.exports = {
     "hyperterm-subpixel-antialiased",
     'hyperterm-1password',
     'hyperterm-summon',
+    "hyper-pane",
   ],
 
   localPlugins: []
