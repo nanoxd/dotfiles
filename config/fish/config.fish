6eb70997ca08345a2ade2dae161ee1eb22320cc7
Fernando Paredes <nano@fdp.io>
2017-08-20 18:25:47 -0700

    [fish] Unset RLS_ROOT

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,7 +9,6 @@ set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.g
 set -x FZF_TMUX 1
 set -x SLACK_URL "https://hooks.slack.com/services/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"
 set -x DYLD_LIBRARY_PATH "$HOME/.rustup/toolchains/nightly-x86_64-apple-darwin"
-set -x RLS_ROOT "$HOME/dev/tools/rls"
 set -x GITHUB_ACCESS_TOKEN (cat ~/.token)
 set -x TEMPLATES "$HOME/dev/templates"
 
