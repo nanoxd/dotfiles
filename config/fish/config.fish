eba9863af0a2d19f33b9ddfc7fb3beb08d21548d
Fernando Paredes <nano@fdp.io>
2017-11-09 10:20:02 -0500

    [config] Remove slack

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,7 +7,6 @@ set -gx PAGER less
 set -x GOPATH "$HOME/dev/go"
 set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
 set -x FZF_TMUX 1
-set -x SLACK_URL "https://hooks.slack.com/services/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"
 set -x DYLD_LIBRARY_PATH "$HOME/.rustup/toolchains/nightly-x86_64-apple-darwin"
 set -x GITHUB_ACCESS_TOKEN (cat ~/.token)
 set -x TEMPLATES "$HOME/dev/templates"
