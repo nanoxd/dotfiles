79b54d07144be2aaff638fe28ee7df210bf4ef7b
Fernando Paredes <nano@fdp.io>
2019-12-28 17:19:46 -0500

    [fish] Resolve bug with Catalina

---
+++ config/fish/functions/__fish_describe_command.fish
@@ -0,0 +1,5 @@
+# Defined in /var/folders/s7/h7h91bnj3kjdzm_4p8jcfj840000gn/T//fish.UvJiDf/__fish_describe_command.fish @ line 2
+function __fish_describe_command
+  # temporary workaround until Catalina fixes the bug
+     # https://github.com/fish-shell/fish-shell/issues/6270
+end
