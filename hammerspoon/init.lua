0327cf4a27434b0b7986ea85cef3b283340bd9b5
Fernando Paredes <nano@fdp.io>
2020-05-02 13:08:00 -0400

    [hammerspoon] Migrate to ReloadConfiguration spoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,1 +1,15 @@
-require 'reload_config'
+-- Load spoons
+hs.loadSpoon("ReloadConfiguration")
+spoon.ReloadConfiguration:start()
+
+local hyper = {"⌘", "⌥", "⌃", "⇧"}
+
+hs.hotkey.bind(hyper, "t", function()
+  local alacritty = hs.application.find('alacritty')
+
+  if alacritty:isFrontmost() then
+    alacritty:hide()
+  else
+    hs.application.launchOrFocus("/Applications/Alacritty.app")
+  end
+end)
