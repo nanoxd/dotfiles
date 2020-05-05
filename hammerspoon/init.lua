0f295cf2e54e76941ea7c1a75cc36b8061b018d0
Fernando Paredes <nano@fdp.io>
2020-05-04 20:17:32 -0400

    [hammerspoon] Add alacritty nil check

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -7,7 +7,7 @@ local hyper = {"⌘", "⌥", "⌃", "⇧"}
 hs.hotkey.bind(hyper, "t", function()
   local alacritty = hs.application.find('alacritty')
 
-  if alacritty:isFrontmost() then
+  if alacritty and alacritty:isFrontmost() then 
     alacritty:hide()
   else
     hs.application.launchOrFocus("/Applications/Alacritty.app")
