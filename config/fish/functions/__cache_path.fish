4be5783ee6595b5022ac8758a99df0e0121fcdd5
Fernando Paredes <nano@fdp.io>
2020-07-27 08:03:30 -0400

    [fish] Clean up fisher files

--- config/fish/functions/__cache_path.fish
+++
@@ -1,4 +0,0 @@
-function __cache_path --argument ns anchor
-	set -l hashed_pwd (__cache_key $anchor); or return 1
-  echo (__cache_dir $ns)"/$hashed_pwd"
-end
--- config/fisher/github.com/nyarly/fish-cache-file/functions/__cache_zap.fish
+++
@@ -1,9 +0,0 @@
-function __cache_zap --description 'Deletes cache files for the current directory under <ns>' --argument ns anchor extra_find
-	set -l constraints ""
-  if [ (count $argv) -gt 2 ]
-    set contraints $argv[3..-1]
-  end
-	for f in (eval find (__cache_dir $ns) -name (__cache_key $anchor) $contraints -print)
-    rm $f
-  end
-end
