a2a3418d8383b70deac936db4b03b2da235aa4b6
Fernando Paredes <nano@fdp.io>
2020-07-27 08:13:12 -0400

    [fish] Remove unused functions

--- config/fish/functions/copy-fish_right_prompt.fish
+++
@@ -1,5 +0,0 @@
-# Display the compressed current working path on the right
-# If the previous command returned any kind of error code, display that too
-
-function fish_right_prompt
-end
--- config/fish/functions/git-remove-db-conflicts.fish
+++
@@ -1,3 +0,0 @@
-function git-remove-db-conflicts -d "Removes Dropbox Conflict nodes"
-  find . -name '*(*conflicted copy*)*' | sed 's/.*/"&"/' | xargs rm 
-end
