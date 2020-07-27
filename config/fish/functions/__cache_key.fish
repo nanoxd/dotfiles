bd23ea62a51726256e62161a1b9f0525eff0ab42
Fernando Paredes <nano@fdp.io>
2020-07-27 08:03:30 -0400

    [fish] Clean up fisher files

--- config/fish/functions/__cache_key.fish
+++
@@ -1,4 +0,0 @@
-function __cache_key --argument anchor
-	set -l anchored_dir (lookup $anchor); or return 1
-  echo $anchored_dir | md5sum --text - | awk '{ print $1 }'
-end
--- config/fish/functions/__cache_or_get.fish
+++
@@ -1,8 +0,0 @@
-function __cache_or_get -a ns anchor get_cmd
-  set -l cache_file (__cache_path $ns $anchor)
-
-  if not test -f "$cache_file"
-    eval $get_cmd > "$cache_file"
-  end
-  cat "$cache_file"
-end
