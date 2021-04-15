f3f2cd066c3cb23f1dd16ab1ef4cba83bf1894b4
Fernando Paredes <nano@fdp.io>
2021-04-14 22:48:43 -0500

    [nvim] Add bindings for Telescope

--- config/nvim/lua/keys.lua
+++ config/nvim/lua/keys.lua
@@ -1,2 +1,6 @@
 local utils = require('utils')
 
+-- Telescope Bindings
+utils.map('n', '<C-p>', '<cmd>Telescope find_files<cr>')
+utils.map('n', '<leader>;', '<cmd>Telescope buffers<cr>')
+utils.map('n', '\\', '<cmd>Telescope live_grep<cr>')
