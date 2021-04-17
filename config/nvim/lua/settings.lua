0c28d9f4705ec7d1a915891ac47ce75a4b3a5590
Fernando Paredes <nano@fdp.io>
2021-04-17 13:35:36 -0500

    [nvim] Transfer remaining settings

--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -1,4 +1,5 @@
 local u = require('utils')
+local cmd = vim.cmd
 
 local function flag(key, scope)
   local s = 'o'
@@ -17,11 +18,52 @@ u.opt('o', 'winwidth', 84)
 u.opt('o', 'winheight', 10)
 u.opt('o', 'winminheight', 10)
 
-u.opt('o', 'background', 'dark')
-vim.g.colors_name = 'tender'
-
 -- Whitespace
 u.opt('w', 'listchars', [[tab:▸\ ,trail:·,eol:¬,nbsp:_]])
 flag('smartindent', 'b')
 u.opt('w', 'wrap', false)
 
+-- Spacing
+u.opt('o', 'tabstop', 2)
+u.opt('o', 'shiftwidth', 2)
+u.opt('o', 'softtabstop', 2)
+flag('expandtab')
+flag('list') -- Show invisible characters
+
+-- Turn off swap files
+u.opt('o', 'backup', false)
+u.opt('o', 'writebackup', false)
+u.opt('o', 'swapfile', false)
+flag('autoread') -- When file is written to outside of vim, read again
+
+-- Persistent Undo
+cmd('silent !mkdir backups > /dev/null 2>&1')
+u.opt('o', 'undodir', 'backups')
+flag('undofile')
+
+-- Display
+u.opt('o', 'background', 'dark')
+vim.g.colors_name = 'tender'
+
+flag('termguicolors') -- Add 24 bit color support
+u.opt('o', 'showmode', false)
+flag('relativenumber')
+flag('number')
+flag('lazyredraw')
+flag('cursorline')
+
+-- Allow Transparency
+cmd('hi! Normal ctermbg=NONE guibg=NONE')
+cmd('hi! NonText ctermbg=NONE guibg=NONE guifg=NONE ctermfg=NONE')
+cmd('highlight link EndOfBuffer Comment')
+
+-- Sensible side scrolling 
+u.opt('o', 'sidescroll', 1)
+u.opt('w', 'sidescrolloff', 3)
+
+-- Search
+flag('ignorecase')
+flag('smartcase')
+
+-- Highlight on Yank
+cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'
