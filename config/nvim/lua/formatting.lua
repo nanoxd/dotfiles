add7e662ac5526441bb5fa1567e3d35356e4bf94
Fernando Paredes <nano@fdp.io>
2021-04-22 20:29:40 -0500

    [nvim] Setup formatting and initial languageserver

---
+++ config/nvim/lua/formatting.lua
@@ -0,0 +1,1 @@
+vim.cmd('autocmd FileType rust set expandtab tabstop=4 softtabstop=4 shiftwidth=4')
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,14 +1,14 @@
-local execute = vim.api.nvim_command
+local cmd = vim.api.nvim_command
 local fn = vim.fn
 
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
-  execute 'packadd packer.nvim'
+  cmd('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+  cmd 'packadd packer.nvim'
 end
 
-vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
+cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
 
 return require('packer').startup(function()
   -- Packer can manage itself
