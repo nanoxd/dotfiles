426fed801e1bb910a72f2a2d68626e894b7b7301
Fernando Paredes <nano@fdp.io>
2021-07-11 20:05:52 -0500

    [hammerspoon] Replace Alacritty with Kitty

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -67,7 +67,30 @@ end
 
 -- Alacritty
 hs.hotkey.bind(utils.hyper, "t", function()
-  launchOrHide("io.alacritty")
+  -- launchOrHide("io.alacritty")
+  launchOrHide("net.kovidgoyal.kitty")
+  local app = hs.application.get("kitty")
+  app:mainWindow():moveToUnit'[100,100,0,0]'
+end)
+
+hs.hotkey.bind(utils.hyper, 'y', function()
+  local app = hs.application.get("kitty")
+
+  if app then
+      if not app:mainWindow() then
+          app:selectMenuItem({"kitty", "New OS window"})
+      elseif app:isFrontmost() then
+          app:hide()
+      else
+          app:activate()
+      end
+  else
+      hs.application.launchOrFocus("kitty")
+      app = hs.application.get("kitty")
+  end
+
+  app:mainWindow():moveToUnit'[100,50,0,0]'
+  app:mainWindow().setShadows(false)
 end)
 
 -- Other Keybindings --
