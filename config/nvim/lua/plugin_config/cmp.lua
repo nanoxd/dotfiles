cc2001a60abb0829a1dae565741e26d685567954
Fernando Paredes <nano@fdp.io>
2021-11-06 10:36:05 -0500

    [nvim] Use tab for completion cycling

--- config/nvim/lua/plugin_config/cmp.lua
+++ config/nvim/lua/plugin_config/cmp.lua
@@ -6,6 +6,7 @@ cmp.setup({
         vim.fn["vsnip#anonymous"](args.body)
       end,
     },
+
     mapping = {
       ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
       ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
@@ -16,13 +17,28 @@ cmp.setup({
         c = cmp.mapping.close(),
       }),
       ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      ["<Tab>"] = function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        else
+          fallback()
+        end
+      end,
+      ["<S-Tab>"] = function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        else
+          fallback()
+        end
+      end,
     },
+
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
       { name = 'vsnip' }, 
-    }, {
       { name = 'buffer' },
     }),
+
     experimental = {
       native_menu = false,
       ghost_text = true,
