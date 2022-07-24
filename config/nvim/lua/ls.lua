074420c0e13ba4bcd091470fb42668e94ef5682d
Fernando Paredes <nano@fdp.io>
2022-07-24 07:41:53 -0500

    [nvim] Use server_capabilities

--- config/nvim/lua/ls.lua
+++ config/nvim/lua/ls.lua
@@ -26,9 +26,9 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', "H", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
 
   -- Set some keybinds conditional on server capabilities
-  if client.resolved_capabilities.document_formatting then
+  if client.server_capabilities.documentFormattingProvider then
     buf_set_keymap('n', "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-  elseif client.resolved_capabilities.document_range_formatting then
+  elseif client.server_capabilities.document_range_formatting then
     buf_set_keymap('n', "<leader>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
   end
 end
