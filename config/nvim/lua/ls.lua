7bf1ab2330a1d05bbfa8f48485fe9134f603ad2c
Fernando Paredes <nano@fdp.io>
2023-04-29 18:13:53 -0500

    [nvim] Update cmp_nvim_lsp syntax

--- config/nvim/lua/ls.lua
+++ config/nvim/lua/ls.lua
@@ -1,5 +1,5 @@
 local lspconfig = require('lspconfig')
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lsp_installer = require("nvim-lsp-installer")
 
 local on_attach = function(client, bufnr)
