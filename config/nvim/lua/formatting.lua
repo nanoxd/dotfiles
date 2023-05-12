8fb2f2cb97fbdcb14081063bb0257083b08f295d
Fernando Paredes <nano@fdp.io>
2023-05-11 20:59:18 -0500

    [nvim] Convert autocmd

--- config/nvim/lua/formatting.lua
+++ config/nvim/lua/formatting.lua
@@ -1,1 +1,4 @@
-vim.cmd('autocmd FileType rust set expandtab tabstop=4 softtabstop=4 shiftwidth=4')
+vim.api.nvim_create_autocmd({'FileType'}, {
+  pattern = 'rust',
+  command = "set expandtab tabstop=4 softtabstop=4 shiftwidth=4"
+})
