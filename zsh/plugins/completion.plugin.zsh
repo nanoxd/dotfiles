a3a5a7db28b1d28002d90cf8e2dfa3636a2e51db
Fernando Paredes <nano@fdp.io>
2023-05-13 11:36:07 -0500

    [zsh] Add fzf-tab

--- zsh/plugins/completion.plugin.zsh
+++ zsh/plugins/completion.plugin.zsh
@@ -33,3 +33,20 @@ setopt auto_menu
 setopt autolist
 setopt menu_complete
 setopt always_to_end
+
+### fzf-tab
+
+# disable sort when completing `git checkout`
+zstyle ':completion:*:git-checkout:*' sort false
+
+# set descriptions format to enable group support
+zstyle ':completion:*:descriptions' format '[%d]'
+
+# set list-colors to enable filename colorizing
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
+# preview directory's content with exa when completing cd
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'
+
+# switch group using `,` and `.`
+zstyle ':fzf-tab:*' switch-group ',' '.'
