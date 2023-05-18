6908f750232eb4abe9fbdd9807ccff5c44074c49
Fernando Paredes <nano@fdp.io>
2023-05-17 21:06:15 -0500

    [zsh] Migrate to zinit (#10)
    * Install zinit
    
    * [zsh] Install syntax, autosuggestions, and completions
    
    * Disable Sheldon
    
    * Move plugins to folder
    
    * Clean up older files

---
+++ zsh/plugins.zsh
@@ -0,0 +1,42 @@
+### Added by Zinit's installer
+if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
+    print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
+    command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
+    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
+        print -P "%F{33} %F{34}Installation successful.%f%b" || \
+        print -P "%F{160} The clone has failed.%f%b"
+fi
+
+source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+
+# Load a few important annexes, without Turbo
+# (this is currently required for annexes)
+zinit light-mode for \
+    zdharma-continuum/zinit-annex-as-monitor \
+    zdharma-continuum/zinit-annex-bin-gem-node \
+    zdharma-continuum/zinit-annex-patch-dl \
+    zdharma-continuum/zinit-annex-rust
+
+### End of Zinit's installer chunk
+
+zinit wait lucid light-mode for \
+  atinit"zicompinit; zicdreplay" \
+      zdharma-continuum/fast-syntax-highlighting \
+  atload"_zsh_autosuggest_start" \
+      zsh-users/zsh-autosuggestions \
+  blockf atpull'zinit creinstall -q .' \
+      zsh-users/zsh-completions
+
+GEOMETRY_COLOR_DIR=152
+zinit ice wait"0" lucid atload"geometry::prompt"
+zinit light geometry-zsh/geometry
+
+zinit light-mode for \
+  Aloxaf/fzf-tab \
+  caarlos0/zsh-mkc \
+  djui/alias-tips \
+  skywind3000/z.lua \
+  RobertAudi/tsm \
+  zsh-users/zsh-history-substring-search
--- zshrc
+++ zshrc
@@ -1,3 +1,13 @@
+source_files_in() {
+    local dir="$1"
+
+    if [[ -d "$dir" && -r "$dir" && -x "$dir" ]]; then
+        for file in "$dir"/*; do
+           [[ -f "$file" && -r "$file" ]] && . "$file"
+        done
+    fi
+}
+
 export ZSH="$HOME/.zsh"
 
 # Add homebrew to path
@@ -9,11 +19,6 @@ if uname -s | grep -q Darwin; then
 fi
 
 export PATH="$HOME/.bin:$HOME/.cargo/bin:$PATH"
-
-eval "$(sheldon source)"
-
-# Pure Prompt
-PURE_CMD_MAX_EXEC_TIME=5
 
 # Android
 ANDROID_DIR="$HOME/Library/Android/sdk/platform-tools"
@@ -38,3 +43,6 @@ export ZSH_PLUGINS_ALIAS_TIPS_REVEAL=1 # Display raw command like in Fish
 if [[ -f "$HOME/.zshrc.local" ]]; then
     source "$HOME/.zshrc.local"
 fi
+
+source "$ZSH/plugins.zsh"
+source_files_in "$ZSH/plugins"
