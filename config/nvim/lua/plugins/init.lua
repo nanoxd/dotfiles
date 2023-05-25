8537347f8246ece3b418544dc1502801b2f73a74
Fernando Paredes <nano@fdp.io>
2023-05-24 22:41:51 -0500

    Move lualine out to file

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -95,20 +95,6 @@ return {
   },
 
   {
-    'nvim-lualine/lualine.nvim',
-    dependencies = {
-      'kyazdani42/nvim-web-devicons', opt = true
-    },
-    config = function()
-      require('lualine').setup {
-        options = {
-          theme = 'palenight',
-        },
-      }
-    end
-  },
-
-  {
     'lewis6991/gitsigns.nvim', -- Adds Git highlighting
     dependencies = {
       'nvim-lua/plenary.nvim'
---
+++ config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,11 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = {
+    'kyazdani42/nvim-web-devicons', opt = true
+  },
+  config = {
+    options = {
+      theme = 'palenight',
+    },
+  },
+}
