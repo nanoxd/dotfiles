6a84e3a3965accb133a2c293083111a419ce0897
Fernando Paredes <nano@fdp.io>
2023-06-04 17:47:49 -0500

    [nvim] Add indent guides

---
+++ config/nvim/lua/plugins/indent-guide.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    event = { "BufReadPost", "BufNewFile" },
+    config = {
+      char = "│",
+      filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+      show_trailing_blankline_indent = false,
+      show_current_context = false,
+    },
+  },
+
+    -- active indent guide and indent text objects
+  {
+    "echasnovski/mini.indentscope",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      -- symbol = "▏",
+      symbol = "│",
+      options = { try_as_border = true },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+  },
+}
