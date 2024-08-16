8bfc4bd7fbd183d59c3f8cf84995860952ff8443
Fernando Paredes <nano@fdp.io>
2024-08-16 16:05:08 -0500

    [hammerspoon] Add wezterm key binding

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -65,33 +65,8 @@ end
 
 -- App Keybindings --
 
---[[ -- Alacritty
-hs.hotkey.bind(utils.hyper, "t", function()
-  -- launchOrHide("io.alacritty")
-  launchOrHide("net.kovidgoyal.kitty")
-  local app = hs.application.get("kitty")
-  app:mainWindow():moveToUnit'[100,100,0,0]'
-end)
-
-hs.hotkey.bind(utils.hyper, 'y', function()
-  local app = hs.application.get("kitty")
-
-  if app then
-      if not app:mainWindow() then
-          app:selectMenuItem({"kitty", "New OS window"})
-      elseif app:isFrontmost() then
-          app:hide()
-      else
-          app:activate()
-      end
-  else
-      hs.application.launchOrFocus("kitty")
-      app = hs.application.get("kitty")
-  end
-
-  app:mainWindow():moveToUnit'[100,50,0,0]'
-  app:mainWindow().setShadows(false)
-end) ]]
+-- Wezterm
+hs.hotkey.bind(utils.hyper, 't', function() launchOrHide 'com.github.wez.wezterm' end)
 
 -- Other Keybindings --
 
