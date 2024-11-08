cabb0da1bca1df39e275d1e315c2d1e7c4593d36
Fernando Paredes <nano@fdp.io>
2024-11-08 11:54:28 -0600

    [nvim] Lint

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,25 +1,26 @@
 cmd = vim.cmd -- to execute Vim commands e.g. cmd('pwd')
 fn = vim.fn -- to call Vim functions e.g. fn.bufnr()
 g = vim.g -- a table to access global variables
-utils = require("utils")
+utils = require 'utils'
 
 -- Map leader to space
-g.mapleader = " "
+g.mapleader = ' '
 g.termguicolors = true -- Add 24 bit color support
+vim.opt.signcolumn = 'yes'
 g.did_load_filetypes = 1
 g.loaded_netrw = 1
 g.loaded_netrwPlugin = 1
 
-require("config.lazy")
-require("keys")
-require("settings")
-require("formatting")
-require("ls")
-require("plugin_config.cmp")
+require 'config.lazy'
+require 'keys'
+require 'settings'
+require 'formatting'
+require 'ls'
+require 'plugin_config.cmp'
 
-local ts = require("nvim-treesitter.configs")
-ts.setup({
-  ensure_installed = { "lua", "rust", "swift", "c", "javascript" },
+local ts = require 'nvim-treesitter.configs'
+ts.setup {
+  ensure_installed = { 'lua', 'rust', 'swift', 'c', 'javascript' },
   highlight = { enable = true },
   rainbow = {
     enable = true,
@@ -35,10 +36,10 @@ ts.setup({
 
       keymaps = {
         -- You can use the capture groups defined in textobjects.scm
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = "@class.inner",
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
 
         -- Or you can define your own textobjects like this
         --[[ ["iF"] = {
@@ -49,4 +50,4 @@ ts.setup({
       },
     },
   },
-})
+}
--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,189 +1,170 @@
 return {
-  "nathom/filetype.nvim",
+  'nathom/filetype.nvim',
 
   -- Better syntax highlighting
   {
-    "nvim-treesitter/nvim-treesitter",
+    'nvim-treesitter/nvim-treesitter',
     dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
+      'nvim-treesitter/nvim-treesitter-textobjects',
     },
-    build = ":TSUpdate",
+    build = ':TSUpdate',
   },
 
   -- LSP and completion
   {
-    "folke/neodev.nvim",
-    config = function()
-      require("neodev").setup({})
-    end,
+    'folke/neodev.nvim',
+    config = function() require('neodev').setup {} end,
   },
-  "neovim/nvim-lspconfig",
+  'neovim/nvim-lspconfig',
   {
-    "williamboman/mason.nvim",
+    'williamboman/mason.nvim',
     dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "neovim/nvim-lspconfig",
+      'williamboman/mason-lspconfig.nvim',
+      'neovim/nvim-lspconfig',
     },
-    build = ":MasonUpdate", -- :MasonUpdate updates registry contents
+    build = ':MasonUpdate', -- :MasonUpdate updates registry contents
     config = function()
-      require("mason").setup()
-      require("mason-lspconfig").setup()
+      require('mason').setup()
+      require('mason-lspconfig').setup()
     end,
   },
   {
-    "hrsh7th/nvim-cmp",
+    'hrsh7th/nvim-cmp',
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "ray-x/cmp-treesitter",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "hrsh7th/cmp-vsnip",
+      'hrsh7th/cmp-nvim-lsp',
+      'ray-x/cmp-treesitter',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-vsnip',
     },
   },
 
-  "onsails/lspkind.nvim",
-  "simrat39/symbols-outline.nvim",
+  'onsails/lspkind.nvim',
+  'simrat39/symbols-outline.nvim',
 
   -- Snippets
-  { "hrsh7th/vim-vsnip", dependencies = { "rafamadriz/friendly-snippets" } },
-  "hrsh7th/vim-vsnip-integ",
-  "hrsh7th/cmp-vsnip",
+  { 'hrsh7th/vim-vsnip', dependencies = { 'rafamadriz/friendly-snippets' } },
+  'hrsh7th/vim-vsnip-integ',
+  'hrsh7th/cmp-vsnip',
 
   -- UI
-  "jacoborus/tender.vim",
+  'jacoborus/tender.vim',
   {
-    "akinsho/bufferline.nvim",
-    dependencies = { "kyazdani42/nvim-web-devicons" },
-    config = function()
-      require("bufferline").setup({})
-    end,
+    'akinsho/bufferline.nvim',
+    dependencies = { 'kyazdani42/nvim-web-devicons' },
+    config = function() require('bufferline').setup {} end,
   },
 
   {
-    "Th3Whit3Wolf/onebuddy",
+    'Th3Whit3Wolf/onebuddy',
     dependencies = {
-      "tjdevries/colorbuddy.vim",
+      'tjdevries/colorbuddy.vim',
     },
-    config = function()
-      require("colorbuddy").colorscheme("onebuddy")
-    end,
+    config = function() require('colorbuddy').colorscheme 'onebuddy' end,
   },
 
   {
-    "lewis6991/gitsigns.nvim", -- Adds Git highlighting
+    'lewis6991/gitsigns.nvim', -- Adds Git highlighting
     dependencies = {
-      "nvim-lua/plenary.nvim",
+      'nvim-lua/plenary.nvim',
     },
-    config = function()
-      require("gitsigns").setup()
-    end,
+    config = function() require('gitsigns').setup() end,
   },
 
   -- Tools
-  "ojroques/nvim-osc52", -- Copy to Clipboard using ANSI OCS52
+  'ojroques/nvim-osc52', -- Copy to Clipboard using ANSI OCS52
   {
-    "norcalli/nvim-colorizer.lua",
-    config = function()
-      require("colorizer").setup()
-    end,
+    'norcalli/nvim-colorizer.lua',
+    config = function() require('colorizer').setup() end,
   },
-  "b3nj5m1n/kommentary", -- Comment mapings
-  "kevinhwang91/nvim-bqf", -- Make QuickFix better
-  "p00f/nvim-ts-rainbow", -- Enable multiple parentheses
-  "rhysd/committia.vim", -- More Pleasant Editing on Commit Message
-  "tpope/vim-unimpaired", -- Add elegant mappings
-  "lambdalisue/suda.vim", -- Sudo support in nvim
-  "tpope/vim-fugitive", -- Git for Vim
+  'b3nj5m1n/kommentary', -- Comment mapings
+  'kevinhwang91/nvim-bqf', -- Make QuickFix better
+  'p00f/nvim-ts-rainbow', -- Enable multiple parentheses
+  'rhysd/committia.vim', -- More Pleasant Editing on Commit Message
+  'tpope/vim-unimpaired', -- Add elegant mappings
+  'lambdalisue/suda.vim', -- Sudo support in nvim
+  'tpope/vim-fugitive', -- Git for Vim
   {
-    "chentoast/marks.nvim", -- Improve marks
+    'chentoast/marks.nvim', -- Improve marks
     config = function()
-      require("marks").setup({
+      require('marks').setup {
         default_mappings = true,
-      })
+      }
     end,
   },
 
   {
-    "numToStr/Navigator.nvim",
+    'numToStr/Navigator.nvim',
     config = function()
-      require("Navigator").setup({
+      require('Navigator').setup {
         disable_on_zoom = true,
-      })
+      }
     end,
   },
 
   {
-    "ur4ltz/surround.nvim",
-    config = function()
-      require("surround").setup({})
-    end,
+    'ur4ltz/surround.nvim',
+    config = function() require('surround').setup {} end,
   },
 
   {
-    "windwp/nvim-autopairs",
-    config = function()
-      require("nvim-autopairs").setup()
-    end,
+    'windwp/nvim-autopairs',
+    config = function() require('nvim-autopairs').setup() end,
   },
 
   {
-    "folke/which-key.nvim",
-    config = function()
-      require("which-key").setup({})
-    end,
+    'folke/which-key.nvim',
+    event = 'VeryLazy',
+    opts = {},
   },
   {
-    "windwp/nvim-spectre",
+    'windwp/nvim-spectre',
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-lua/popup.nvim",
+      'nvim-lua/plenary.nvim',
+      'nvim-lua/popup.nvim',
     },
   },
 
   {
-    "folke/trouble.nvim",
-    dependencies = "kyazdani42/nvim-web-devicons",
-    config = function()
-      require("trouble").setup({})
-    end,
+    'folke/trouble.nvim',
+    dependencies = 'kyazdani42/nvim-web-devicons',
+    config = function() require('trouble').setup {} end,
   },
 
   {
-    "AckslD/nvim-neoclip.lua",
-    dependencies = { "tami5/sqlite.lua", module = "sqlite" },
-    config = function()
-      require("neoclip").setup()
-    end,
+    'AckslD/nvim-neoclip.lua',
+    dependencies = { 'tami5/sqlite.lua', module = 'sqlite' },
+    config = function() require('neoclip').setup() end,
   },
 
-  "mrjones2014/smart-splits.nvim",
+  'mrjones2014/smart-splits.nvim',
   {
-    "mrcjkb/rustaceanvim",
-    version = "^5", -- Recommended
+    'mrcjkb/rustaceanvim',
+    version = '^5', -- Recommended
     lazy = false, -- This plugin is already lazy
   },
 
   {
-    "stevearc/conform.nvim",
+    'stevearc/conform.nvim',
     opts = {},
     config = function()
-      require("conform").setup({
+      require('conform').setup {
         formatters_by_ft = {
-          lua = { "stylua" },
+          lua = { 'stylua' },
           -- Conform will run multiple formatters sequentially
-          python = { "isort", "black" },
+          python = { 'isort', 'black' },
           -- You can customize some of the format options for the filetype (:help conform.format)
-          rust = { "rustfmt", lsp_format = "fallback" },
+          rust = { 'rustfmt', lsp_format = 'fallback' },
           -- Conform will run the first available formatter
-          javascript = { "prettierd", "prettier", stop_after_first = true },
+          javascript = { 'prettierd', 'prettier', stop_after_first = true },
         },
         format_on_save = {
           -- These options will be passed to conform.format()
           timeout_ms = 500,
-          lsp_format = "fallback",
+          lsp_format = 'fallback',
         },
-      })
+      }
     end,
   },
 }
