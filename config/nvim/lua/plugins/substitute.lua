968a1c3e68da6cc795fd6f6ef9a62ff849c2fec0
Fernando Paredes <nano@fdp.io>
2024-11-08 16:13:56 -0600

    [nvim] Add substitute plugin

---
+++ config/nvim/lua/plugins/substitute.lua
@@ -0,0 +1,14 @@
+return {
+  'gbprod/substitute.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    local substitute = require 'substitute'
+
+    substitute.setup()
+
+    vim.keymap.set('n', 's', substitute.operator, { desc = 'Substitute with motion' })
+    vim.keymap.set('n', 'ss', substitute.line, { desc = 'Substitute line' })
+    vim.keymap.set('n', 'S', substitute.eol, { desc = 'Substitute to end of line' })
+    vim.keymap.set('x', 's', substitute.visual, { desc = 'Substitute in visual mode' })
+  end,
+}
