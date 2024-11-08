8f537ffa2517ba9e6bede125a4f4cb16e07a0988
Fernando Paredes <nano@fdp.io>
2024-11-08 11:55:16 -0600

    [nvim] Update telescope

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,24 +1,35 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    cmd = "Telescope",
-    version = false,
-    dependencies = {
-      "nvim-lua/popup.nvim",
-      "nvim-lua/plenary.nvim",
-    },
-    keys = {
-      { "<C-p>", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-      { "<leader>;", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-      { "\\", "<cmd>Telescope live_grep<cr>", desc = "Grep in directory" },
-      { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-    },
+  'nvim-telescope/telescope.nvim',
+  cmd = 'Telescope',
+  branch = '0.1.x',
+  dependencies = {
+    'nvim-lua/popup.nvim',
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
-  {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-    config = function()
-      require("telescope").load_extension("fzf")
-    end,
+  keys = {
+    { '<C-p>', '<cmd>Telescope find_files<cr>', desc = 'Find files' },
+    { '<leader>;', '<cmd>Telescope buffers<cr>', desc = 'Buffers' },
+    { '\\', '<cmd>Telescope live_grep<cr>', desc = 'Grep in directory' },
+    { '<leader>:', '<cmd>Telescope command_history<cr>', desc = 'Command History' },
   },
+  config = function()
+    local telescope = require 'telescope'
+    local actions = require 'telescope.actions'
+
+    telescope.setup {
+      defaults = {
+        path_display = { 'smart' },
+        mappings = {
+          i = {
+            ['<C-k>'] = actions.move_selection_previous, -- move to prev result
+            ['<C-j>'] = actions.move_selection_next, -- move to next result
+            ['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
+          },
+        },
+      },
+    }
+
+    telescope.load_extension 'fzf'
+  end,
 }
