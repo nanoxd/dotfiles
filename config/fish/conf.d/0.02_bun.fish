2407be1e6cc69506b7d20191765f1d6356e40a2c
Fernando Paredes <nano@fdp.io>
2024-11-08 23:44:51 -0600

    [fish] Fix bun function

--- config/fish/conf.d/0.02_bun.fish
+++ config/fish/conf.d/0.02_bun.fish
@@ -1,5 +1,6 @@
-if command -q bun
-    set -Ux BUN_INSTALL "$HOME/.bun"
+set -Ux BUN_INSTALL "$HOME/.bun"
+
+if test -d $BUN_INSTALL/bin
     fish_add_path -amg "$BUN_INSTALL/bin"
 else
     _warn_no_command bun
