65eb0a50d05faceed9fe5ece92800ab5cf40ccb2
Fernando Paredes <nano@fdp.io>
2024-11-24 22:25:27 -0600

    [nvim] Move conform to opt and use event

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -86,24 +86,22 @@ return {
 
   {
     'stevearc/conform.nvim',
-    opts = {},
-    config = function()
-      require('conform').setup {
-        formatters_by_ft = {
-          lua = { 'stylua' },
-          -- Conform will run multiple formatters sequentially
-          python = { 'isort', 'black' },
-          -- You can customize some of the format options for the filetype (:help conform.format)
-          rust = { 'rustfmt', lsp_format = 'fallback' },
-          -- Conform will run the first available formatter
-          javascript = { 'prettierd', 'prettier', stop_after_first = true },
-        },
-        format_on_save = {
-          -- These options will be passed to conform.format()
-          timeout_ms = 500,
-          lsp_format = 'fallback',
-        },
-      }
-    end,
+    opts = {
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- Conform will run multiple formatters sequentially
+        python = { 'isort', 'black' },
+        -- You can customize some of the format options for the filetype (:help conform.format)
+        rust = { 'rustfmt', lsp_format = 'fallback' },
+        -- Conform will run the first available formatter
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      },
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_format = 'fallback',
+      },
+    },
+    event = { 'BufWritePre' },
   },
 }
