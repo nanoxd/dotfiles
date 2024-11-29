c3e93b644a0fcfd878873c16717bbf17559727fd
Fernando Paredes <nano@fdp.io>
2024-11-28 23:28:32 -0600

    [wezterm] Use kanagawa theme

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -18,7 +18,28 @@ end
 local config = wezterm.config_builder()
 config.default_cwd = wezterm.home_dir
 
-config.color_scheme = 'Tango (terminal.sexy)'
+config.force_reverse_video_cursor = true
+-- Kanagawa
+config.colors = {
+  foreground = '#dcd7ba',
+  background = '#1f1f28',
+
+  cursor_bg = '#c8c093',
+  cursor_fg = '#c8c093',
+  cursor_border = '#c8c093',
+
+  selection_fg = '#c8c093',
+  selection_bg = '#2d4f67',
+
+  scrollbar_thumb = '#16161d',
+  split = '#16161d',
+
+  ansi = { '#090618', '#c34043', '#76946a', '#c0a36e', '#7e9cd8', '#957fb8', '#6a9589', '#c8c093' },
+  brights = { '#727169', '#e82424', '#98bb6c', '#e6c384', '#7fb4ca', '#938aa9', '#7aa89f', '#dcd7ba' },
+  indexed = { [16] = '#ffa066', [17] = '#ff5d62' },
+}
+
+-- config.color_scheme = 'Tango (terminal.sexy)'
 config.max_fps = 120
 
 config.hide_tab_bar_if_only_one_tab = false
