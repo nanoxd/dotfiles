9a8d04054a1a8b078643e94b54e0919adff585d4
Fernando Paredes <nano@fdp.io>
2024-11-29 00:23:05 -0600

    [nvim] Set vim-mode to abbreviated term

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,23 @@
+local mode_map = {
+  ['COMMAND'] = '!',
+  ['CONFIRM'] = 'Y?',
+  ['EX'] = 'X',
+  ['INSERT'] = 'I',
+  ['MORE'] = 'M',
+  ['NORMAL'] = 'N',
+  ['O-PENDING'] = 'N?',
+  ['REPLACE'] = 'R',
+  ['S-BLOCK'] = 'SB',
+  ['S-LINE'] = 'SL',
+  ['SELECT'] = 'S',
+  ['SHELL'] = 'SH',
+  ['TERMINAL'] = 'T',
+  ['V-BLOCK'] = 'VB',
+  ['V-LINE'] = 'VL',
+  ['V-REPLACE'] = 'VR',
+  ['VISUAL'] = 'V',
+}
+
 return {
   'nvim-lualine/lualine.nvim',
   event = 'VeryLazy',
@@ -5,9 +25,19 @@ return {
     'kyazdani42/nvim-web-devicons',
     opt = true,
   },
-  config = {
-    options = {
-      theme = 'auto',
-    },
-  },
+  config = function()
+    require('lualine').setup {
+      options = {
+        theme = 'auto',
+      },
+      sections = {
+        lualine_a = {
+          {
+            'mode',
+            fmt = function(s) return mode_map[s] or s end,
+          },
+        },
+      },
+    }
+  end,
 }
